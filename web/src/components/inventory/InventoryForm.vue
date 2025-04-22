ebd5e7e2ea9133bf2bc36e0e727bfdd5bd688175
kawa <weihomeben@gmail.com>
2025-04-23 03:17:41 +0800

    fix: 修正 QR code 圖片 URL 處理邏輯 | Fix QR code image URL handling logic

--- web/src/components/inventory/InventoryForm.vue
+++ web/src/components/inventory/InventoryForm.vue
@@ -224,7 +224,7 @@
             >
               <div class="qrcode-image">
                 <img
-                  :src="qrcode.qrcode_url"
+                  :src="qrcode.qrcode_url.startsWith('http') ? qrcode.qrcode_url : `${API_BASE_URL}${qrcode.qrcode_url}`"
                   :alt="qrcode.name"
                 />
               </div>
